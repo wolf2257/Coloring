cb823e91a9aa70bf686fde72272855ac03f37d79
Tokino <tjddlf2645@gmail.com>
2015-05-19 22:26:45 +0900

    TestLevel + Level editor templete

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -52,23 +52,67 @@ bool HelloWorld::init()
 	auto sprite = Sprite::create("gamescene/background/3.png");
 	sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 
-	auto tb = CBrickTable::Create(5, 3, Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+	auto sprite1 = Sprite::create("gamescene/character/char.png");
+	sprite1->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 
-	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 0, 0);
-	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 1, 0);
-	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 2, 0);
-	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 3, 0);
-	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 4, 0);
+	auto tb = CBrickTable::Create(18, 10, Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 
-	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 0, 1);
-	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 1, 1);
-	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 2, 1);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 14, 1);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 15, 1);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 16, 1);
 
-	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 0, 2);
-	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 1, 2);
-	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 2, 2);
-	
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 0, 2);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 1, 2);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 2, 2);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 3, 2);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 4, 2);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 5, 2);
+
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 0, 3);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 1, 3);
+
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 0, 4);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 1, 4);
+
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 0, 5);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 1, 5);
+
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 0, 6);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 1, 6);
+
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 0, 7);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 1, 7);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 10, 7);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 11, 7);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 12, 7);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 13, 7);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 17, 7);
+
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 0, 8);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 1, 8);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 10, 8);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 11, 8);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 12, 8);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 13, 8);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 17, 8);
+
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 0, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 1, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 2, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 3, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 4, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 5, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 6, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 7, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Glass), 8, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 10, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 11, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 12, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 13, 9);
+	tb->AddBrick(CBrick::Create(CBrick::BrickType::Empty), 17, 9);
+
 	this->addChild(sprite, 0);
+	this->addChild(sprite1, 2);
 	tb->AttatchAll(this, 1);
 
     return true;
